45c86158482cf074f0a6f7f3f2458386b7ddeeee
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-06-16 17:58:23 -0700

    gsap animation library install and text animation feat.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // 1.Importing our own JS files
 
-import darkMode from './modules/darkMode';
+import darkMode from "./modules/darkMode";
+import animate from "./modules/animate";
 
-   import '../scss/main.scss';
+import "../scss/main.scss";
--- src/js/modules/DOMelements.js
+++ src/js/modules/DOMelements.js
@@ -1,4 +1,5 @@
 export const elements = {
-  bodyTag: document.querySelector('body'),
-  darkModeBtn: document.querySelector('button'),
+  bodyTag: document.querySelector("body"),
+  darkModeBtn: document.querySelector("button"),
+  words: document.querySelectorAll(".word"),
 };
