de90eaea579a4e039fcae358c9a52542782d786e
Jacob Martinez <jaccob.alxndr@gmail.com>
2020-07-15 14:45:07 -0700

    adjusting mobile styles

--- src/js/modules/animateText.js
+++ src/js/modules/animateText.js
@@ -1,19 +1,19 @@
-// 0. Imports
-import { elements } from "./DOMelements";
-import { gsap } from "gsap";
-// 1. Selectors
-const { words } = elements;
-words.forEach((word) => {
-  const HTML = [...word.textContent]
-    .map((char) => `<span class='letter'>${char}</span>`)
-    .join("");
-  word.innerHTML = HTML;
-});
+// // 0. Imports
+// import { elements } from "./DOMelements";
+// import { gsap } from "gsap";
+// // 1. Selectors
+// const { words } = elements;
+// words.forEach((word) => {
+//   const HTML = [...word.textContent]
+//     .map((char) => `<span class='letter'>${char}</span>`)
+//     .join("");
+//   word.innerHTML = HTML;
+// });
 
-// 2. Actions
-gsap.to(".letter", {
-  duration: 1,
-  y: 4,
-  stagger: { amount: -0.5, repeat: -1, yoyo: true },
-  ease: "power1.inOut",
-});
+// // 2. Actions
+// gsap.to(".letter", {
+//   duration: 1,
+//   y: 4,
+//   stagger: { amount: -0.5, repeat: -1, yoyo: true },
+//   ease: "power1.inOut",
+// });
