1d42fe680f146d416bf8801eab06c6faf82ba164
Jacob Martinez <jacobmartinez@Jacobs-MBP.attlocal.net>
2023-05-05 16:03:54 -0700

    feat: added past agencies links

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const config = require('./webpack.config');
+const path = require("path");
+const merge = require("webpack-merge");
+const config = require("./webpack.config");
 
 module.exports = merge(config, {
-  mode: 'development', // 'production' || 'development' || 'none'
-  devtool: 'eval-source-map',
+  mode: "development", // 'production' || 'development' || 'none'
+  devtool: "eval-source-map",
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -15,10 +15,10 @@ module.exports = merge(config, {
         // scss rule
         test: /\.scss$/,
         use: [
-          'style-loader', // Creates `style` nodes from JS strings
-          'css-loader', // Translates CSS into CommonJS
-          'postcss-loader',
-          'sass-loader', // Compiles Sass to CSS
+          "style-loader", // Creates `style` nodes from JS strings
+          "css-loader", // Translates CSS into CommonJS
+          "postcss-loader",
+          "sass-loader", // Compiles Sass to CSS
         ],
       },
     ],
